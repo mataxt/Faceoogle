f941042b744a65c886eddb004e36924ec9788e31
Rami <rami.alsawadi1@gmail.com>
2015-11-03 20:22:01 +0100

    User.java
    gersda

---
+++ Faceoogle/src/model/User.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USER_TABLE")
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String username;
+	private String name;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
