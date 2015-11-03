2d7db8a3927bfcc5f50d15b2ed3cd7069fe3b430
Rami <rami.alsawadi1@gmail.com>
2015-11-03 22:32:33 +0100

    User Annotations

--- Faceoogle/src/model/User.java
+++ Faceoogle/src/model/User.java
@@ -1,32 +1,58 @@
 package model;
 
 import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="USER_TABLE")
+@Table(name="Users")
 
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String username;
+	private String password;
 	private String name;
+	private Date birthDate;
+	private String gender;
 	
+	@Id
+	@Column(name="Username")
 	public String getUsername() {
 		return username;
 	}
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	@Column(name="Password")
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Column(name="Name")
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
+	@Column(name="BirthDate")
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	@Column(name="Gender")
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 }
