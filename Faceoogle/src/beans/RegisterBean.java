5eadd4e54b6e64efddfdb03139db1f9985011c38
Rami <rami.alsawadi1@gmail.com>
2015-11-04 20:53:39 +0100

    register bean n stuff
    vegen

---
+++ Faceoogle/src/beans/RegisterBean.java
@@ -0,0 +1,58 @@
+package beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+
+
+
+@ManagedBean(name="registerBean")
+public class RegisterBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private String username;
+	private String password;
+	private String name;
+	private Date birthDate;
+	private String gender;
+	
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void register(){
+		System.out.println(this.name +"\n" +
+				 this.birthDate+ "\n" + this.gender+
+				this.username +"\n");
+	}
+
+
+}
--- Faceoogle/src/model/User.java
+++ Faceoogle/src/model/User.java
@@ -1,7 +1,7 @@
 package model;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
