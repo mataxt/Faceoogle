d7b3d72a49f7892dd90c198b8c97e49687ee7c0b
Mathias <mataxt@kth.se>
2015-11-06 11:37:52 +0100

    More minor layout fixes and added functionality
    More minor layout fixes and added functionality

--- Faceoogle/src/beans/MessagesBean.java
+++ Faceoogle/src/beans/MessagesBean.java
@@ -8,5 +8,31 @@ import javax.faces.bean.ManagedBean;
 public class MessagesBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	// ...
+	private String to;
+	private String title;
+	private String message;
+	
+	public String getTo() {
+		return to;
+	}
+	
+	public void setTo(String to) {
+		this.to = to;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
