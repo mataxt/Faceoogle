2abd3a174a3bcbc6e8dc17356dc9dbfd466b46e0
Mathias <mataxt@kth.se>
2015-11-06 16:25:11 +0100

    xhtml fixes and added new SQL Query
    xhtml fixes and added new SQL Query

--- Faceoogle/src/database/UserDB.java
+++ Faceoogle/src/database/UserDB.java
@@ -21,7 +21,6 @@ public class UserDB {
 		}
 
 		emf.close();
-
 	}
 
 	public static boolean checkUser(User usr) {
@@ -29,15 +28,18 @@ public class UserDB {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UserPU");
 		EntityManager em = emf.createEntityManager();
 		try {
-			String u = (String) em.createQuery("SELECT password FROM users as users WHERE users.username = ?1 ")
-					.setParameter(1, usr.getUsername()).getSingleResult();
-			System.out.println(u);
+			//String u = (String) em.createQuery("SELECT password FROM users as users WHERE users.username = ?1 ").setParameter(1, usr.getUsername()).getSingleResult();
+			//em.createQuery("SELECT password FROM User as users WHERE User.username = ?1 ").setParameter(1, usr.getUsername()).getSingleResult();
+			String query = "from User where username = '" + usr.getUsername() + "' and password = '" + usr.getPassword() + "'";
+			
+			User u = em.createQuery(query, User.class).getSingleResult();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			em.close();
 		}
 		emf.close();
+		
 		return false;
 	}
 }
